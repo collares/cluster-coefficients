a5b9189e5c4a112867a0588f1a9c6ba4c6dc60f9
Mauricio Collares <mauricio@collares.org>
2024-06-06 21:00:08 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "cluster-compute";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              pkg-config autoconf automake ginac boost
+            ];
+          };
+        }
+      );
+}
