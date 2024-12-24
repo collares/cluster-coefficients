f855ec50691d3c3b2339633a79139642bd06c0b7
Mauricio Collares <mauricio@collares.org>
2024-12-24 14:35:58 +0100

    Bump to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cluster-coefficients";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
